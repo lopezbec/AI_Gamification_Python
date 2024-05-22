3e69465b1e78b476127f3e9362397bbd87b82315
Prisila E. Báez Méndez <prisi.e.baez@gmail.com>
2024-05-21 20:01:44 -0400

    refactor badge_criteria_streak file #79

--- Elmer/Daniel_JSON_Files_Elmer/badge_system/badge_criteria_streak.py
+++ Elmer/Daniel_JSON_Files_Elmer/badge_system/badge_criteria_streak.py
@@ -2,6 +2,7 @@ import json
 import os
 from badge_system.badge_verification import BadgeVerification
 
+
 class BadgeCriteriaStreak:
     def __init__(self):
         self.streak = 0
@@ -15,9 +16,72 @@ class BadgeCriteriaStreak:
     def get_current_streak(self):
         return self.streak
 
-def display_badge(badge_details):
-    badge_window = BadgeVerification(badge_details)
-    badge_window.exec()
+def read_stored_streak(username):
+    # Nombre del directorio
+    directory = 'badge_progress'
+    # Nombre del archivo json
+    filename = f'{username}_badge_progress.json'
+    # Path completo al archivo
+    filepath = os.path.join(directory, filename)
+
+    # Verificar si el archivo json existe
+    if os.path.exists(filepath):
+        # Leer el contenido del archivo json
+        with open(filepath, 'r') as file:
+            # Cargar los datos del archivo JSON
+            badge_data = json.load(file)
+            return badge_data['streak']
+    else:
+        print(f"El archivo {filename} no existe.")
+        return None
+
+def reset_streak(username):
+    # Nombre del directorio
+    directory = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'badge_progress')
+    # Nombre del archivo json
+    filename = f'{username}_badge_progress.json'
+    # Path completo al archivo
+    filepath = os.path.join(directory, filename)
+
+    # Verificar si el archivo json existe
+    if os.path.exists(filepath):
+        # Leer el contenido del archivo json
+        with open(filepath, 'r') as file:
+            # Cargar los datos del archivo JSON
+            badge_data = json.load(file)
+        
+        # Actualizar el valor de streak en el diccionario
+        badge_data['streak'] = 0
+        
+        # Escribir los datos actualizados en el archivo json
+        with open(filepath, 'w') as file:
+            json.dump(badge_data, file, indent=4)
+    else:
+        print(f"El archivo {filename} no existe.")
+
+def update_streak(username, new_streak):
+    # Nombre del directorio
+    directory = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'badge_progress')
+    # Nombre del archivo json
+    filename = f'{username}_badge_progress.json'
+    # Path completo al archivo
+    filepath = os.path.join(directory, filename)
+
+    # Verificar si el archivo json existe
+    if os.path.exists(filepath):
+        # Leer el contenido del archivo json
+        with open(filepath, 'r') as file:
+            # Cargar los datos del archivo JSON
+            badge_data = json.load(file)
+        
+        # Actualizar el valor de streak en el diccionario
+        badge_data['streak'] += new_streak
+        
+        # Escribir los datos actualizados en el archivo json
+        with open(filepath, 'w') as file:
+            json.dump(badge_data, file, indent=4)
+    else:
+        print(f"El archivo {filename} no existe.")
 
 def load_badges():
     with open("badge_info.json", "r") as file:
@@ -32,6 +96,10 @@ def check_badges(streak):
     for criteria in badges_criteria:
         if streak >= criteria.get("value", 0):
             display_badge(criteria.get("badge_id"))
+
+def display_badge(badge_details):
+    badge_window = BadgeVerification(badge_details)
+    badge_window.exec()
 
 def save_badge_progress_per_user(username):
     # Nombre del directorio
@@ -64,70 +132,3 @@ def save_badge_progress_per_user(username):
             json.dump(badge_data, file, indent=4)
     else:
         pass
-
-def update_streak(username, new_streak):
-    # Nombre del directorio
-    directory = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'badge_progress')
-    # Nombre del archivo json
-    filename = f'{username}_badge_progress.json'
-    # Path completo al archivo
-    filepath = os.path.join(directory, filename)
-
-    # Verificar si el archivo json existe
-    if os.path.exists(filepath):
-        # Leer el contenido del archivo json
-        with open(filepath, 'r') as file:
-            # Cargar los datos del archivo JSON
-            badge_data = json.load(file)
-        
-        # Actualizar el valor de streak en el diccionario
-        badge_data['streak'] += new_streak
-        
-        # Escribir los datos actualizados en el archivo json
-        with open(filepath, 'w') as file:
-            json.dump(badge_data, file, indent=4)
-    else:
-        print(f"El archivo {filename} no existe.")
-
-def reset_streak(username):
-    # Nombre del directorio
-    directory = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'badge_progress')
-    # Nombre del archivo json
-    filename = f'{username}_badge_progress.json'
-    # Path completo al archivo
-    filepath = os.path.join(directory, filename)
-
-    # Verificar si el archivo json existe
-    if os.path.exists(filepath):
-        # Leer el contenido del archivo json
-        with open(filepath, 'r') as file:
-            # Cargar los datos del archivo JSON
-            badge_data = json.load(file)
-        
-        # Actualizar el valor de streak en el diccionario
-        badge_data['streak'] = 0
-        
-        # Escribir los datos actualizados en el archivo json
-        with open(filepath, 'w') as file:
-            json.dump(badge_data, file, indent=4)
-    else:
-        print(f"El archivo {filename} no existe.")
-
-def read_stored_streak(username):
-    # Nombre del directorio
-    directory = 'badge_progress'
-    # Nombre del archivo json
-    filename = f'{username}_badge_progress.json'
-    # Path completo al archivo
-    filepath = os.path.join(directory, filename)
-
-    # Verificar si el archivo json existe
-    if os.path.exists(filepath):
-        # Leer el contenido del archivo json
-        with open(filepath, 'r') as file:
-            # Cargar los datos del archivo JSON
-            badge_data = json.load(file)
-            return badge_data['streak']
-    else:
-        print(f"El archivo {filename} no existe.")
-        return None
