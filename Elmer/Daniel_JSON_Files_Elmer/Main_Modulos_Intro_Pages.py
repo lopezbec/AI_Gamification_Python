a9fb67f3af115875d6ebece839340a6d20bd91a3
Christian Lopez <54818552+lopezbec@users.noreply.github.com>
2024-08-15 08:28:34 -0400

    Update Main_Modulos_Intro_Pages.py

--- Elmer/Daniel_JSON_Files_Elmer/Main_Modulos_Intro_Pages.py
+++ Elmer/Daniel_JSON_Files_Elmer/Main_Modulos_Intro_Pages.py
@@ -135,7 +135,7 @@ class MainWindow(QtWidgets.QMainWindow):
         modulos_layout = QtWidgets.QHBoxLayout(self.modulos_menu_widget)
 
         # Crear y añadir los botones de módulo al layout de módulos
-        self.modulo1_btn = self.setup_modulos_menu("Módulo 1", 5)
+        self.modulo1_btn = self.setup_modulos_menu("Modulo 1", 5)
         self.modulo2_btn = self.setup_modulos_menu("Módulo 2", 3)
         self.modulo3_btn = self.setup_modulos_menu("Módulo 3", 5)
         self.modulo4_btn = self.setup_modulos_menu("Módulo 4", 5)
