f0131057179eb7c6abf119d2fbe15ef2a47fae75
Jose Matias <josealfredomatias493@gmail.com>
2025-02-16 10:58:27 -0400

    Integrating Congratulation_Feature

--- Elmer/Daniel_JSON_Files_Elmer/Main_Modulos_Quizzes_Window.py
+++ Elmer/Daniel_JSON_Files_Elmer/Main_Modulos_Quizzes_Window.py
@@ -8,6 +8,8 @@ from PyQt6.QtCore import Qt
 from drag_drop import DraggableLabel, DropLabel
 from Codigos_LeaderBoard.Main_Leaderboard_FV import LeaderBoard
 from Main_Modulos_Intro_Pages import MainWindow as Dashboard
+from Elmer.Daniel_JSON_Files_Elmer.congratulation_Feature import CongratulationWindow
+
 
 
 class JsonLoader:
@@ -448,6 +450,7 @@ class QuizLoader:
             self.feedback_label.setText('¡Correcto!')
             self.feedback_label.setStyleSheet(
                 f"color: {self.styles.get('correct_color', '#00FF00')}; font-size: {self.styles.get('font_size_answers', 12)}px")
+            CongratulationWindow.correct_response()
             self.submit_button.setVisible(False)
             if self.is_last_section():
                 self.complete_button.setVisible(True)
@@ -457,6 +460,7 @@ class QuizLoader:
             self.feedback_label.setText('Incorrecto, inténtalo de nuevo.')
             self.feedback_label.setStyleSheet(
                 f"color: {self.styles.get('incorrect_color', '#FF0000')}; font-size: {self.styles.get('font_size_answers', 12)}px")
+            CongratulationWindow.incorrect_response()
 
     def check_multiple_choice_answers(self):
         selected_answers = [btn.text() for btn in self.button_widgets if btn.isChecked()]
@@ -466,6 +470,7 @@ class QuizLoader:
             self.feedback_label.setText('¡Correcto!')
             self.feedback_label.setStyleSheet(
                 f"color: {self.styles.get('correct_color', '#00FF00')}; font-size: {self.styles.get('font_size_answers', 12)}px")
+            CongratulationWindow.correct_response()
             self.submit_button.setVisible(False)
             if self.is_last_section():
                 self.complete_button.setVisible(True)
@@ -475,6 +480,7 @@ class QuizLoader:
             self.feedback_label.setText('Incorrecto, inténtalo de nuevo.')
             self.feedback_label.setStyleSheet(
                 f"color: {self.styles.get('incorrect_color', '#FF0000')}; font-size: {self.styles.get('font_size_answers', 12)}px")
+            CongratulationWindow.incorrect_response()
 
     def check_complete_blank_space_answers(self):
         try:
@@ -518,6 +524,7 @@ class QuizLoader:
                 self.feedback_label.setText("¡Correcto!")
                 self.feedback_label.setStyleSheet(
                     f"color: {self.styles.get('correct_color', '#00FF00')}; font-size: {self.styles.get('font_size_answers', 12)}px")
+                CongratulationWindow.correct_response()
                 self.submit_button.setVisible(False)
                 if self.is_last_section():
                     self.complete_button.setVisible(True)
@@ -528,6 +535,7 @@ class QuizLoader:
                 self.feedback_label.setText("Respuesta incorrecta. Inténtalo de nuevo.")
                 self.feedback_label.setStyleSheet(
                     f"color: {self.styles.get('incorrect_color', '#FF0000')}; font-size: {self.styles.get('font_size_answers', 12)}px")
+                CongratulationWindow.incorrect_response()
         except Exception as e:
             print(f"Error al verificar respuestas de completeblankspace: {e}")
 
